93f04b651fab23415fbe707f8ffe02300fe1285b
ojef1 <ojefdev@gmail.com>
2023-03-02 11:15:48 -0300

    seção dados da empresa,animação de loader, e parallax

---
+++ js/scripts.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+    //Progress bar
+    let containerA =document.getElementById("circleA")
+    
+    let circleA = new ProgressBar.Circle(containerA, {
+
+        color:'#64daf9',
+        strokeWidth: 5,
+        duration: 1400,
+        from: {color: '#aaa'},
+        to: {color: '#65daf9'},
+        
+        step: function(state, circle){
+            circle.path.setAttribute('stroke',state.color);
+
+            let value= Math.round(circle.value() * 60);
+            
+            circle.setText(value);
+        }
+    })
+
+    let containerB =document.getElementById("circleB")
+    
+    let circleB = new ProgressBar.Circle(containerB, {
+
+        color:'#64daf9',
+        strokeWidth: 5,
+        duration: 1600,
+        from: {color: '#aaa'},
+        to: {color: '#65daf9'},
+        
+        step: function(state, circle){
+            circle.path.setAttribute('stroke',state.color);
+
+            let value= Math.round(circle.value() * 254);
+            
+            circle.setText(value);
+        }
+    })
+
+    let containerC =document.getElementById("circleC")
+    
+    let circleC = new ProgressBar.Circle(containerC, {
+
+        color:'#64daf9',
+        strokeWidth: 5,
+        duration: 2000,
+        from: {color: '#aaa'},
+        to: {color: '#65daf9'},
+        
+        step: function(state, circle){
+            circle.path.setAttribute('stroke',state.color);
+
+            let value= Math.round(circle.value() * 32);
+            
+            circle.setText(value);
+        }
+    })
+
+    let containerD =document.getElementById("circleD")
+    
+    let circleD = new ProgressBar.Circle(containerD, {
+
+        color:'#64daf9',
+        strokeWidth: 5,
+        duration: 2000,
+        from: {color: '#aaa'},
+        to: {color: '#65daf9'},
+        
+        step: function(state, circle){
+            circle.path.setAttribute('stroke',state.color);
+
+            let value= Math.round(circle.value() * 5243);
+            
+            circle.setText(value);
+        }
+    })
+
+    //iniciando o loader quando o usuário chega no elemento
+    let dataAreaOffset= $('#data-area').offset();
+    let stop = 0;
+
+    $(window).scroll(function(e){
+        let scroll = $(window).scrollTop();
+
+        if (scroll > (dataAreaOffset.top - 500)&& stop ===0){
+            circleA.animate(1.0)
+            circleB.animate(1.0)
+            circleC.animate(1.0)
+            circleD.animate(1.0)
+
+            stop = 1; 
+        }
+    })
+
+
+    //Parallax
+    setTimeout(function(){
+
+        $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'})
+
+    }, 250)
+
+    
+})
